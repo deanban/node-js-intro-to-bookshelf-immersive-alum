4f791a80e46747b88e613de6e6afafd9fac12462
github username <github email address>
2018-07-11 23:21:16 -0400

    migration done

--- migrations/20160518124958_blog.js
+++ migrations/20160518124958_blog.js
@@ -1,10 +1,42 @@
 exports.up = function(knex, Promise) {
-  knex.schema.createTable("users").then(tb1 => {
-    tb1.increments();
-    tb1.string("name");
+  knex.schema.createTable("users").then(userTabl => {
+    userTabl.increments();
+    userTabl.string("name");
+    userTabl.string("username");
+    userTabl.string("email");
+  });
+  knex.schema.createTable("posts").then(postsTabl => {
+    postsTabl.increments();
+    postsTabl.string("title");
+    postsTabl.string("body");
+    postsTabl
+      .integer("user_id")
+      .references("id")
+      .inTable("users")
+      .notNull()
+      .onDelete("cascade");
+  });
+  knex.schema.createTable("comments").then(commTabl => {
+    commTabl.increments();
+    //   commTabl.string("title");
+    commTabl.string("body");
+    commTabl
+      .integer("user_id")
+      .references("id")
+      .inTable("users")
+      .notNull()
+      .onDelete("cascade");
+    commTabl
+      .integer("post_id")
+      .references("id")
+      .inTable("posts")
+      .notNull()
+      .onDelete("cascade");
   });
 };
 
 exports.down = function(knex, Promise) {
-  knex.schema.destroyTable("users");
+  knex.schema.dropTable("users");
+  knex.schema.dropTable("posts");
+  knex.schema.dropTable("comments");
 };
