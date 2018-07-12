9a702276bd32c124d64d25ccc08b9893e948d37b
github username <github email address>
2018-07-11 22:49:06 -0400

    halfway

--- migrations/20160518124958_blog.js
+++ migrations/20160518124958_blog.js
@@ -1,8 +1,10 @@
-
 exports.up = function(knex, Promise) {
-  
+  knex.schema.createTable("users").then(tb1 => {
+    tb1.increments();
+    tb1.string("name");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  knex.schema.destroyTable("users");
 };
